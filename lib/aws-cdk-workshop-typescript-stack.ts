0b4c296ad6dd2e63718a0f7e6f8f460c59f28da6
d-shimizu <2187802+d-shimizu@users.noreply.github.com>
2024-02-23 08:07:58 +0000

    RDS DB インスタンスのデプロイ

--- lib/aws-cdk-workshop-typescript-stack.ts
+++ lib/aws-cdk-workshop-typescript-stack.ts
@@ -1,9 +1,15 @@
-import { CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // ec2 パッケージをインポート
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+// rds のパッケージを import
+import * as rds from "aws-cdk-lib/aws-rds";
+// elb のパッケージを import
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
 
-import { readFileSync } from "fs";
+// 自作コンストラクタを import
+import { WebServerInstance } from "./constructs/web-server-instance";
 
 export class AwsCdkWorkshopTypescriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,30 +20,54 @@ export class AwsCdkWorkshopTypescriptStack extends Stack {
 		ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
 	});
 
-	// EC2 インスタンスの宣言を準備
-	const webServer1 = new ec2.Instance(this, "WordpressServer1", {
-		// EC2 を起動する VPC を設定
+	// 新しく作成したコンストラクトを使用してインスタンスを宣言
+	const webServer1 = new WebServerInstance(this, "WordpressServer1", {
 		vpc,
-		// インスタンスタイプを設定
-		instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-		// AmazonLinuxImage インスタンスを生成し、AMI を設定
-		machineImage: new ec2.AmazonLinuxImage({
-			generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-		}),
-		// EC2 インスタンスを配置するサブネットを設定
-		vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
 	});
 
-	// user-data.sh を読み込み、変数に格納
-	const script = readFileSync("./lib/resources/user-data.sh", "utf8");
-	webServer1.addUserData(script);
+	// 2つ目のインスタンスを宣言
+	const webServer2 = new WebServerInstance(this, "WordpressServer2", {
+		vpc,
+	});
 
-	// port 80, 全ての IP アドレスからのアクセスを許可
-	webServer1.connections.allowFromAnyIpv4(ec2.Port.tcp(80));
+	// RDS のインスタンスを宣言
+	const dbServer = new rds.DatabaseInstance(this, "WordpressDB", {
+		vpc,
+		// DatabaseInstanceEngine クラスを利用してデータベースエンジンを設定
+		engine: rds.DatabaseInstanceEngine.mysql({version: rds.MysqlEngineVersion.VER_8_0_31 }),
+		// RDS DB インスタンスのインスタンスタイプを設定
+		instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
+		// RDS DB インスタンスのデータベース名を設定
+		databaseName: "wordpress",
+	    // multiAZ を true へ
+        multiAz: true,
+	});
 
-	// EC2 インスタンスアクセス用の IP アドレスを出力
-    new CfnOutput(this, "WordpressServer1PublicIPAddress", {
-      value: `http://${webServer1.instancePublicIp}`,
-    });
+	// Web サーバーから RDS DB サーバーへのアクセスを許可
+	dbServer.connections.allowDefaultPortFrom(webServer1.instance);
+
+	const alb = new elbv2.ApplicationLoadBalancer(this, "LoadBalancer", {
+		vpc,
+		internetFacing: true,
+	});
+
+	const listener = alb.addListener("Listener", {
+		port: 80,
+	});
+
+	listener.addTargets("ApplicationFleet", {
+		port: 80,
+		targets: [
+		  new targets.InstanceTarget(webServer1.instance, 80),
+		  // ターゲットに 2 台目のインスタンスを追加
+		  new targets.InstanceTarget(webServer2.instance, 80)
+		],
+		healthCheck: {
+			path: "/wp-includes/images/blank.gif",
+		},
+	});
+
+	webServer1.instance.connections.allowFrom(alb, ec2.Port.tcp(80));
+	webServer2.instance.connections.allowFrom(alb, ec2.Port.tcp(80));
   }
 }
